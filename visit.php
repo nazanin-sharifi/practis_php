3ac9241f238c9ec5d243504b9f49912d7f046168
Nazanin sharifi | Wesiter <143528080+nazanin-sharifi@users.noreply.github.com>
2025-12-06 19:16:51 +0330

    Counting the number of user visits
    Ability to count the number of times a user visits a page using sessions

--- visit.php
+++ visit.php
@@ -1,1 +1,19 @@
+<?php
+session_start();
+if(isset($_SESSION['counter'])){
+    $_SESSION['counter'] +=1;
+}
+else{
+    $_SESSION['counter']=1;
+}
 
+$msg = "شما ازین صفحه به تعداد". $_SESSION['counter'];
+$msg .= "بازدید داشته اید!";
+
+?>
+
+<title>تعداد بازدید کاربر</title>
+
+<?php
+each($msg)
+?>
